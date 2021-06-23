3e711a5d0682fd2630a836dffa6473db51058b84
BrittP-NBCC <britt.goguen@gmail.com>
2021-06-23 00:02:53 -0300

    Setting up basic project
    initial commit. Started windows forms, adding basic components

---
+++ WeightLossHelper/WeightLossHelper/frmHome.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace WeightLossHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblCurrentWeek = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Location = new System.Drawing.Point(662, 0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(38, 35);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "X";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Firebrick;
+            this.panel1.Controls.Add(this.lblCurrentWeek);
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(728, 128);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblCurrentWeek
+            // 
+            this.lblCurrentWeek.AutoSize = true;
+            this.lblCurrentWeek.Location = new System.Drawing.Point(324, 22);
+            this.lblCurrentWeek.Name = "lblCurrentWeek";
+            this.lblCurrentWeek.Size = new System.Drawing.Size(35, 13);
+            this.lblCurrentWeek.TabIndex = 1;
+            this.lblCurrentWeek.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 498);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblCurrentWeek;
+    }
+}
+
---
+++ WeightLossHelper/WeightLossHelper/frmWeekly.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace WeightLossHelper
+{
+    partial class frmWeekly
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCurrentWeek = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCurrentWeek
+            // 
+            this.lblCurrentWeek.AutoSize = true;
+            this.lblCurrentWeek.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrentWeek.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblCurrentWeek.Location = new System.Drawing.Point(279, 42);
+            this.lblCurrentWeek.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.lblCurrentWeek.Name = "lblCurrentWeek";
+            this.lblCurrentWeek.Size = new System.Drawing.Size(100, 30);
+            this.lblCurrentWeek.TabIndex = 2;
+            this.lblCurrentWeek.Text = "Week 5";
+            this.lblCurrentWeek.Click += new System.EventHandler(this.lblCurrentWeek_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(279, 171);
+            this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 30);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Week 5";
+            // 
+            // frmWeekly
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(676, 361);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCurrentWeek);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmWeekly";
+            this.Text = "frmWeekly";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCurrentWeek;
+        private System.Windows.Forms.Label label1;
+    }
+}
